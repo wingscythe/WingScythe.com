8e7747d49998172c0d66743184995a97fd44b016
Xurya <xur1@bxscience.edu>
2020-07-22 18:29:18 -0400

    Massive Project Update

--- script.js
+++ script.js
@@ -61,6 +61,9 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
+    if(slides.length==0){
+        return;
+    }
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
